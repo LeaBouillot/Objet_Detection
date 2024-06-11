5a01463b688710163038214379e92944d1526e67
bestdevelope <leabouillot7@gmail.com>
2024-06-11 23:39:41 +0200

    Changement de input image avec FileReader

---
+++ script.js
@@ -0,0 +1,48 @@
+/* PROJET CANVAS
+
+Creer un input de type file en HTML
+
+Lors du changement de cet input, avec FileReader, afficher cette image dans un canvas sur votre HTML
+
+Ensuite, utiliser le canvas avec TensorFlow pour detecter les objets dans l'image :
+https://github.com/tensorflow/tfjs-models/tree/master/coco-ssd
+
+Le model coco-ssd va permettre de faire des predictions sur la présence d'objets sur l'image et donner leurs positions
+
+https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+https://developer.mozilla.org/fr/docs/Web/API/CanvasRenderingContext2D/drawImage
+*/
+
+// Recuperer le fichier de l'input et le fichier
+const inputImage = document.getElementById('image');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+// récupérer l'image de l'input de type file en javascript grace a un addEventListener('change')
+inputImage.addEventListener('change', () => {
+    const file = inputImage.files [0];
+    if (!file) {
+        return;
+    }
+    const reader= new FileReader();
+
+    reader.addEventListener('load', () => {
+        img.src = reader.result; //stocker le resultat du chargement dans la src de img
+        //charger image
+        img.addEventListener('load', async () => {
+
+        });
+            
+        });
+    });
+
+
+})
+    
+
+
+
+
+
+
